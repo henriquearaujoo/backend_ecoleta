3023e0ef8e38f500ee6f428a2be5cf3aaaaebf5b
Henrique Araújo <henriquearaujoo@gmail.com>
2020-06-07 22:28:46 -0400

    Upload de arquivos

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,39 @@
 import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 routes.get('/items', ItemsController.index);
 routes.get('/points', PointsController.index);
 routes.get('/points/:id', PointsController.show);
-routes.post('/points', PointsController.create);
+
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    },
+  ),
+  PointsController.create,
+);
 
 export default routes;
